2e45b463d084f5bfbeb5c24457c54d88c5833338
Dimitris Loukas <dimlucas@outlook.com>
2017-08-25 23:41:39 +0300

    - Add app routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
+
+let routes: Routes = [
+    { path: '', component: AppComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {SharedModule} from "./shared/shared.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
     declarations: [
@@ -10,7 +11,8 @@ import {SharedModule} from "./shared/shared.module";
     ],
     imports: [
         BrowserModule,
-        SharedModule
+        SharedModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
